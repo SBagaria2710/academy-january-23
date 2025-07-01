611b24d829bc5f65c6d1e7f2d12375277c9ae4c7
Shashwat Bagaria <shashwawtforwork@gmail.com>
2025-07-01 22:42:10 +0530

    Theatre APIs

--- bms-project/server/server.js
+++ bms-project/server/server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const app = express();
 const userRouter = require("./routes/userRoutes");
 const movieRouter = require("./routes/movieRoutes");
+const theatreRouter = require("./routes/theatreRoutes");
 
 require("dotenv").config();
 require("./config/db");
@@ -12,6 +13,7 @@ app.use(cors());
 
 app.use("/api/users", userRouter);
 app.use("/api/movies", movieRouter);
+app.use("/api/theatres", theatreRouter);
 
 app.get("/health", (req, res) => {
   res.status(200).send("Service is healthy!");
